64c4c277b93e1033735d6caad5580efe5916a2bf
Manikantareddy1 <51731900+Manikantareddy1@users.noreply.github.com>
2021-07-20 15:19:24 +0530

    mani1

--- app/src/main/java/com/arr/indianbankandroid/RegistrationActivity.java
+++ app/src/main/java/com/arr/indianbankandroid/RegistrationActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+    TextInputEditText rName,rFname,rDob,rOccupation,rPnumber,rEmail,rAddress,rCity,rPan,rAdhar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-    }
+
+        rName = findViewById(R.id.);
+        rFname = findViewById(R.id.);
+        rDob = findViewById(R.id.);
+        rOccupation = findViewById(R.id.);
+        rPnumber = findViewById(R.id.);
+        rEmail = findViewById(R.id.);
+        rAddress = findViewById(R.id.);
+        rCity = findViewById(R.id.);
+        rPan = findViewById(R.id.);
+        rAdhar = findViewById(R.id.);
+
 }
